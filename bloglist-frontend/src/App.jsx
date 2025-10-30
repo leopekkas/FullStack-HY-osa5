615ca7eb5a295b5a2e26282b7103c39c68033bde
leopekkas <leopekkas@gmail.com>
2025-10-30 23:26:06 +0200

    Add ex 5.1.

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState, useEffect } from 'react'
+import LoginForm from './components/LoginForm'
+import loginService from './services/login'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,11 +14,33 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (credentials) => {
+    try {
+      const user = await loginService.login(credentials)
+      setUser(user)
+    } catch (exception) {
+      alert('wrong username or password')
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <LoginForm handleLogin={handleLogin} />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
+
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <div key={blog.id}>
+          {blog.title} {blog.author}
+        </div>
       )}
     </div>
   )
